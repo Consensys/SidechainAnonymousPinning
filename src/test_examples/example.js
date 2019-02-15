f6e48276a51eeea1634705a219bd61119a5d123d
Peter Robinson <drinkcoffee@eml.cc>
2019-02-15 13:25:24 +1000

    Initial version

--- src/test_examples/example.js
+++ src/test_examples/example.js
@@ -20,8 +20,8 @@ var web3 = new Web3();
 
 const VotingAlgMajority = artifacts.require("./VotingAlgMajority.sol");
 
-contract('Voting: types of voting / things to vote on:', function(accounts) {
-    let common = require('./common');
+contract('Examples:', function(accounts) {
+    let common = require('../test/common');
 
     const A_SIDECHAIN_ID = "0x001d3b0000000000030040000000000006400000000000000000000000000002";
 
@@ -29,7 +29,6 @@ contract('Voting: types of voting / things to vote on:', function(accounts) {
     async function addSidechain(pinningInterface) {
         await pinningInterface.addSidechain(A_SIDECHAIN_ID, (await VotingAlgMajority.deployed()).address, common.VOTING_PERIOD);
     }
-
 
     it("add a masked participant", async function() {
         let pinningInterface = await common.getNewAnonPinning();
@@ -58,7 +57,7 @@ contract('Voting: types of voting / things to vote on:', function(accounts) {
 
 
 
-    it("add a  unmasked participant", async function() {
+    it("add an unmasked participant", async function() {
         let pinningInterface = await common.getNewAnonPinning();
         await addSidechain(pinningInterface);
 
@@ -79,32 +78,8 @@ contract('Voting: types of voting / things to vote on:', function(accounts) {
         assert.equal(newParticipant, newParticipantStored, "unexpectedly, the stored participant did not match the value supplied.");
     });
 
-    it("add a masked participant", async function() {
-        let pinningInterface = await common.getNewAnonPinning();
-        await addSidechain(pinningInterface);
 
-        let newParticipant = accounts[1];
-        let salt = "0x123456789ABCDEF0123456789ABCDEF0";
-        let maskedParticipant  = web3.utils.keccak256(newParticipant, salt);
-
-        await pinningInterface.proposeVote(A_SIDECHAIN_ID, common.VOTE_ADD_MASKED_PARTICIPANT, maskedParticipant, "0", "0");
-        await common.mineBlocks(parseInt(common.VOTING_PERIOD));
-        await pinningInterface.actionVotes(A_SIDECHAIN_ID, maskedParticipant);
-        const result = await common.checkVotingResult(pinningInterface);
-        assert.equal(true, result, "incorrect result reported in event");
-
-        let isParticipant = await pinningInterface.isSidechainParticipant.call(A_SIDECHAIN_ID, maskedParticipant);
-        assert.equal(isParticipant, false, "unexpectedly, New masked participant: isSidechainParticipant != false");
-
-        let numMaskedParticipant = await pinningInterface.getMaskedSidechainParticipantsSize.call(A_SIDECHAIN_ID);
-        assert.equal(numMaskedParticipant, 1, "unexpectedly, number of masked participants != 1");
-
-        let maskedParticipantStored = await pinningInterface.getMaskedSidechainParticipant.call(A_SIDECHAIN_ID, "0");
-        let maskedParticipantStoredHex = web3.utils.toHex(maskedParticipantStored);
-        assert.equal(maskedParticipant, maskedParticipantStoredHex, "unexpectedly, the stored masked participant did not match the value supplied.");
-    });
-
-    it("remove an  unmasked participant", async function() {
+    it("remove an unmasked participant", async function() {
         let pinningInterface = await common.getNewAnonPinning();
         await addSidechain(pinningInterface);
 
@@ -178,18 +153,25 @@ contract('Voting: types of voting / things to vote on:', function(accounts) {
     });
 
 
-    it("reject a PIN", async function() {
+    it("add a pin and then reject a pin", async function() {
         let pinningInterface = await common.getNewAnonPinning();
         await addSidechain(pinningInterface);
 
-        let randomSeed = "0";   // this should be random
-        let randValue1 = web3.utils.keccak256(randomSeed);
-        let randValue2 = web3.utils.keccak256(randValue1);
-        let randValue3 = web3.utils.keccak256(randValue2);
-        let randValue4 = web3.utils.keccak256(randValue3);
-        //console.log("RandValue1: " + randValue1);
-        //console.log("RandValue2: " + randValue2);
-        //console.log("RandValue3: " + randValue3);
+        let seed = 0; // This value should be randomly generated and sent to all nodes.
+        let prfResult = await common.prfInit(seed);
+        //console.log("prfResult: " + prfResult);
+        prfResult = await common.prfNextValue(prfResult[0], prfResult[1]);
+        let randValue1 = prfResult[2];
+        prfResult = await common.prfNextValue(prfResult[0], prfResult[1]);
+        let randValue2 = prfResult[2];
+        prfResult = await common.prfNextValue(prfResult[0], prfResult[1]);
+        let randValue3 = prfResult[2];
+        prfResult = await common.prfNextValue(prfResult[0], prfResult[1]);
+        let randValue4 = prfResult[2];
+        // console.log("RandValue1: " + randValue1);
+        // console.log("RandValue2: " + randValue2);
+        // console.log("RandValue3: " + randValue3);
+        // console.log("RandValue3: " + randValue4);
 
         let initialPin = "0x0000000000000000000000000000000000000000000000000000000000000000";
         let blockHash0 = "0x00000000000000000000000000000000000000000000000000000000001a3450";
@@ -219,28 +201,26 @@ contract('Voting: types of voting / things to vote on:', function(accounts) {
 
 
 
-         await pinningInterface.addPin(calculatedPinKey0, blockHash0);
-         await pinningInterface.addPin(calculatedPinKey1, blockHash1);
-         await pinningInterface.addPin(calculatedPinKey2, blockHash2);
+        await pinningInterface.addPin(calculatedPinKey0, blockHash0);
+        await pinningInterface.addPin(calculatedPinKey1, blockHash1);
+        await pinningInterface.addPin(calculatedPinKey2, blockHash2);
 
-         const retrievedPin = await pinningInterface.getPin(calculatedPinKey2);
-         assert.equal(blockHash2, retrievedPin);
+        const retrievedPin = await pinningInterface.getPin(calculatedPinKey2);
+        assert.equal(blockHash2, retrievedPin);
 
-         const retrievedPinNonExistent = await pinningInterface.getPin(calculatedPinKey3);
-         assert.equal("0x0000000000000000000000000000000000000000000000000000000000000000", retrievedPinNonExistent);
+        const retrievedPinNonExistent = await pinningInterface.getPin(calculatedPinKey3);
+        assert.equal("0x0000000000000000000000000000000000000000000000000000000000000000", retrievedPinNonExistent);
 
 
         await pinningInterface.proposeVote(A_SIDECHAIN_ID, common.VOTE_CONTEST_PIN,
-             calculatedPinKey2,  // Pin being contested
-             calculatedPinKey1,  // Previous Pin
-             randValue3);        // Psuedo random function value demonstrating that the pins are connected and belong to sidechain 2.
+            calculatedPinKey2,  // Pin being contested
+            calculatedPinKey1,  // Previous Pin
+            randValue3);        // Psuedo random function value demonstrating that the pins are connected and belong to sidechain 2.
         await common.mineBlocks(parseInt(common.VOTING_PERIOD));
         await pinningInterface.actionVotes(A_SIDECHAIN_ID, calculatedPinKey2);
         const result1 = await common.checkVotingResult(pinningInterface);
         assert.equal(true, result1, "incorrect result reported in event");
-
-
-
     });
+
 
 });
