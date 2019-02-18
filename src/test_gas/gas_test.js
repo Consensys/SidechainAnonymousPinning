fef7c149c007b758d4fa7f83936244b2ee23571b
Peter Robinson <drinkcoffee@eml.cc>
2019-02-18 12:05:43 +1000

    Fixed gas tests: remove participants tests now working

--- src/test_gas/gas_test.js
+++ src/test_gas/gas_test.js
@@ -94,36 +94,37 @@ contract('Pinning Gas Tests:', function(accounts) {
         let maskedParticipantStored = await pinningInterface.getMaskedSidechainParticipant.call(A_SIDECHAIN_ID, EXPECTED_OFFSET);
         let maskedParticipantStoredHex = web3.utils.toHex(maskedParticipantStored);
         assert.equal(maskedParticipant3, maskedParticipantStoredHex, "unexpectedly, the stored masked participant did not match the value supplied.");
+        // I don't know why, but this is throwing a revert.
         // result = await pinningInterface.unmask(A_SIDECHAIN_ID, EXPECTED_OFFSET, salt, {from: newParticipant3});
         // console.log("Unmask: " + result.receipt.gasUsed);
 
 
 
-        // let participantToRemove = newParticipant2;
-        // result = await pinningInterface.proposeVote(A_SIDECHAIN_ID, common.VOTE_REMOVE_UNMASKED_PARTICIPANT, participantToRemove, "0", "0");
-        // console.log("Propose VOTE_ADD_MASKED_PARTICIPANT: " + result.receipt.gasUsed);
-        // result = await pinningInterface.vote(A_SIDECHAIN_ID, common.VOTE_REMOVE_UNMASKED_PARTICIPANT, participantToRemove, true, {from: newParticipant1});
-        // console.log("Propose VOTE_REMOVE_UNMASKED_PARTICIPANT: " + result.receipt.gasUsed);
-        // await common.mineBlocks(parseInt(common.VOTING_PERIOD));
-        // result = await pinningInterface.actionVotes(A_SIDECHAIN_ID, participantToRemove);
-        // console.log("Action  VOTE_REMOVE_UNMASKED_PARTICIPANT: " + result.receipt.gasUsed);
-        // let result2 = await common.checkVotingResult(pinningInterface);
-        // assert.equal(true, result2, "incorrect result reported in event");
-        // numUnmaskedParticipants = await pinningInterface.getUnmaskedSidechainParticipantsSize.call(A_SIDECHAIN_ID);
-        // console.log("Num Unmasked Participants: " + numUnmaskedParticipants);
+        // Remove the unmasked participant.
+        let participantToRemove = newParticipant2;
+        const EXPECTED_OFFSET_PARTICIPANT2 = "2";
+        result = await pinningInterface.proposeVote(A_SIDECHAIN_ID, common.VOTE_REMOVE_UNMASKED_PARTICIPANT, participantToRemove, EXPECTED_OFFSET_PARTICIPANT2, "0");
+        console.log("Propose VOTE_REMOVE_UNMASKED_PARTICIPANT: " + result.receipt.gasUsed);
+        result = await pinningInterface.vote(A_SIDECHAIN_ID, common.VOTE_REMOVE_UNMASKED_PARTICIPANT, participantToRemove, true, {from: newParticipant1});
+        console.log("Vote    VOTE_REMOVE_UNMASKED_PARTICIPANT: " + result.receipt.gasUsed);
+        await common.mineBlocks(parseInt(common.VOTING_PERIOD));
+        result = await pinningInterface.actionVotes(A_SIDECHAIN_ID, participantToRemove);
+        console.log("Action  VOTE_REMOVE_UNMASKED_PARTICIPANT: " + result.receipt.gasUsed);
 
-        //
-        // // Remove the participant.
-        // const EXPECTED_OFFSET = "0";
-        // result = await pinningInterface.proposeVote(A_SIDECHAIN_ID, common.VOTE_REMOVE_MASKED_PARTICIPANT, maskedParticipant1, EXPECTED_OFFSET, "0");
-        // console.log("Propose VOTE_REMOVE_MASKED_PARTICIPANT: " + result.receipt.gasUsed);
-        // await common.mineBlocks(parseInt(common.VOTING_PERIOD));
-        // result = await pinningInterface.actionVotes(A_SIDECHAIN_ID, maskedParticipant);
-        // console.log("Action  VOTE_REMOVE_MASKED_PARTICIPANT: " + result.receipt.gasUsed);
-        // result2 = await common.checkVotingResult(pinningInterface);
-        // assert.equal(true, result2, "incorrect result reported in event");
-        //
-        //
+        isParticipant = await pinningInterface.isSidechainParticipant.call(A_SIDECHAIN_ID, participantToRemove);
+        assert.equal(isParticipant, false, "unexpectedly, Removed Participant: isSidechainParticipant == true");
+        numUnmaskedParticipants = await pinningInterface.getUnmaskedSidechainParticipantsSize.call(A_SIDECHAIN_ID);
+        console.log("Num Unmasked Participants: " + numUnmaskedParticipants);
+
+        // Remove the masked participant.
+        const EXPECTED_OFFSET_MASKED = "0";
+        result = await pinningInterface.proposeVote(A_SIDECHAIN_ID, common.VOTE_REMOVE_MASKED_PARTICIPANT, maskedParticipant3, EXPECTED_OFFSET_MASKED, "0");
+        console.log("Propose VOTE_REMOVE_MASKED_PARTICIPANT: " + result.receipt.gasUsed);
+        await common.mineBlocks(parseInt(common.VOTING_PERIOD));
+        result = await pinningInterface.actionVotes(A_SIDECHAIN_ID, maskedParticipant3);
+        console.log("Action  VOTE_REMOVE_MASKED_PARTICIPANT: " + result.receipt.gasUsed);
+
+
         let seed = 0; // This value should be randomly generated and sent to all nodes.
         let prfResult = await common.prfInit(seed);
         //console.log("prfResult: " + prfResult);
@@ -193,14 +194,70 @@ contract('Pinning Gas Tests:', function(accounts) {
     }
 
 
+    async function addSidechain(pinningInterface) {
+        await pinningInterface.addSidechain(A_SIDECHAIN_ID, (await VotingAlgMajority.deployed()).address, common.VOTING_PERIOD);
+    }
+
+    async function addMaskedParticipant(pinningInterface, participant, salt) {
+        var Web3 = require('web3');
+        var web3 = new Web3();
+
+//        let maskedParticipant  = web3.utils.keccak256("0x000000000000000000000000" + participant.substring(2) + salt);
+        let maskedParticipant  = web3.utils.keccak256(participant + salt);
+
+        await pinningInterface.proposeVote(A_SIDECHAIN_ID, common.VOTE_ADD_MASKED_PARTICIPANT, maskedParticipant, "0", "0");
+        await common.mineBlocks(parseInt(common.VOTING_PERIOD));
+        await pinningInterface.actionVotes(A_SIDECHAIN_ID, maskedParticipant);
+        const result = await common.checkVotingResult(pinningInterface);
+        assert.equal(true, result, "incorrect result reported in event");
+
+        return maskedParticipant;
+    }
+
+
+    it("unmask", async function() {
+        var Web3 = require('web3');
+        var web3 = new Web3();
+
+        let pinningInterface = await await common.getNewAnonPinning();
+        await addSidechain(pinningInterface);
+
+        let newParticipant = accounts[1];
+        let salt1 = "0000000000000000000000000000000000000000000000000000000000000001";
+        let salt = "0x" + salt1;
+        let maskedParticipant  = await addMaskedParticipant(pinningInterface, newParticipant, salt1);
+
+        // Check that the masked participant exists and the unmasked participant does not exist.
+        let isParticipant = await pinningInterface.isSidechainParticipant.call(A_SIDECHAIN_ID, newParticipant);
+        assert.equal(isParticipant, false, "unexpectedly, New masked participant: isSidechainParticipant != false");
+
+        const EXPECTED_OFFSET = "0";
+        let maskedParticipantStored = await pinningInterface.getMaskedSidechainParticipant.call(A_SIDECHAIN_ID, EXPECTED_OFFSET);
+        let maskedParticipantStoredHex = web3.utils.toHex(maskedParticipantStored);
+        assert.equal(maskedParticipant, maskedParticipantStoredHex, "unexpectedly, the stored masked participant did not match the value supplied.");
+
+
+        result = await pinningInterface.unmask(A_SIDECHAIN_ID, EXPECTED_OFFSET, salt, {from: newParticipant});
+        console.log("Unmask: " + result.receipt.gasUsed);
+
+        // Check that the masked participant doesn't exist and the unmasked participant does exist.
+        isParticipant = await pinningInterface.isSidechainParticipant.call(A_SIDECHAIN_ID, newParticipant);
+        assert.equal(isParticipant, true, "unexpectedly, unmasked participant doesnt exist: isSidechainParticipant == false");
+
+        maskedParticipantStored = await pinningInterface.getMaskedSidechainParticipant.call(A_SIDECHAIN_ID, EXPECTED_OFFSET);
+        maskedParticipantStoredHex = web3.utils.toHex(maskedParticipantStored);
+        assert.equal("0x0", maskedParticipantStoredHex, "unexpectedly, the stored masked participant not zeroized.");
+    });
+
+
 
     it("Pinning V1", async function() {
-       let pinInstance = await SidechainAnonPinningV1.new((await VotingAlgMajority.deployed()).address, common.VOTING_PERIOD, common.PIN_CONTEST_PERIOD);
-       let receipt = await web3.eth.getTransactionReceipt(pinInstance.transactionHash);
-       console.log("gas: contract deploy: " + receipt.gasUsed);
+        let pinInstance = await SidechainAnonPinningV1.new((await VotingAlgMajority.deployed()).address, common.VOTING_PERIOD, common.PIN_CONTEST_PERIOD);
+        let receipt = await web3.eth.getTransactionReceipt(pinInstance.transactionHash);
+        console.log("gas: contract deploy: " + receipt.gasUsed);
 
-       let pinningInterface = await common.getNewAnonPinning();
-       await tests(pinningInterface);
+        let pinningInterface = await common.getNewAnonPinning();
+        await tests(pinningInterface);
     });
 
 
