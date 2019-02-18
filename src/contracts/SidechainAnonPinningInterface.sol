97fb88d2a4ca7fed458d20b90d43f58f5a7af9db
Peter Robinson <drinkcoffee@eml.cc>
2019-02-18 12:04:06 +1000

    Improved documentation plus added getPinDisputePeriod

--- src/contracts/SidechainAnonPinningInterface.sol
+++ src/contracts/SidechainAnonPinningInterface.sol
@@ -16,10 +16,10 @@ pragma solidity ^0.4.23;
 /**
  * Contract to manage multiple sidechains.
  *
+ * _Masked and Unmasked Participants_
  * For each sidechain, there are masked and unmasked participants. Unmasked Participants have their
- * addresses listed as being members of a certain sidechain. Being unmasked allows the participant
- * to vote to add and remove other participants, change the voting period and algorithm, and contest
- * pins.
+ * addresses listed as being members of the sidechain. Being unmasked allows the participant
+ * to vote to add and remove other participants and contest pins.
  *
  * Masked Participants are participants which are listed against a sidechain. They are represented
  * as a salted hash of their address. The participant keeps the salt secret and keeps it off-chain.
@@ -27,38 +27,43 @@ pragma solidity ^0.4.23;
  * sending address to create the salted hash. If this matches their masked participant value then
  * they become an unmasked participant.
  *
+ * _Voting_
  * Voting works in the following way:
  * - An Unmasked Participant of a sidechain can submit a proposal for a vote for a certain action
  *  (VOTE_REMOVE_MASKED_PARTICIPANT,VOTE_ADD_UNMASKED_PARTICIPANT, VOTE_REMOVE_UNMASKED_PARTICIPANT,
- *  VOTE_CHANGE_VOTING_ALG, VOTE_CHANGE_VOTING_PERIOD, VOTE_CHANGE_PIN_VOTING_ALG,
- *  VOTE_CHANGE_PIN_VOTING_PERIOD).
+ *  VOTE_CONTEST_PIN).
  * - Any other Unmasked Participant can then vote on the proposal.
  * - Once the voting period has expired, any Unmasked Participant can request the vote be actioned.
  *
- * The voting algorithm is configurable and set on a per-sidechain basis.
+ * The voting algorithm and voting period are configurable and set on a per-sidechain basis at time of
+ * construction.
  *
- * Pinning values are put into a map. All participants of a sidechain agree on a sidechain secret.
- * The sidechain secret seeds a Deterministic Random Bit Generator (DRBG). A new 256 bit value is
+ * _Management Pseudo Chain_
+ * In addition to the normal sidechains, there is a Management Pseudo Chain (sidechain ID 0x00). This
+ * sidechain is automatically created at contract deployment time. Any unmasked participant of this
+ * sidechain can add a new sidechain.
+ *
+ * _Pinning_
+ * Pinning values are put into a map. All participants of a sidechain agree on a sidechain secret
+ * and a Pseudo Random Function (PRF) algorithm. The sidechain secret seeds the PRF. A new 256 bit value is
  * generated each time an uncontested pin is posted. The key in the map is calculated using the
  * equation:
  *
- * DRBG_Value = DRBG.nextValue
- * Key = keccak256(Sidechain Identifier, Previous Pin, DRBG_Value).
+ * PRF_Value = PRF.nextValue
+ * Key = keccak256(Sidechain Identifier, Previous Pin, PRF_Value).
  *
  * For the initial key for a sidechain, the Previous Pin is 0x00.
- * The DRBG algorithm needs to be agreed between participants. Algorithms from SP800-90 are known to
- * be good algorithms.
  *
  * Masked and unmasked participants of a sidechain observe the pinning map at the Key value waiting
  * for the next pin to be posted to that entry in the map. When the pin value is posted, they can then
  * determine if they wish to contest the pin. To contest the pin, they submit:
  *
  * Previous Key (and hence the previous pin)
- * DRBG_Value
+ * PRF_Value
  * Sidechain Id
  *
- * Given they know the valid DRBG Value, they are able to contest the pin, because they must be a member of the
- * sidechain. Given a good DRBG algorithm, this will not expose future or previous DRBG values, and hence will
+ * Given they know the valid PRF Value, they are able to contest the pin, because they must be a member of the
+ * sidechain. Given a good PRF algorithm, this will not expose future or previous PRF values, and hence will
  * not reveal earlier or future pinning values, and hence won't reveal the transaction rate of the sidechain.
  *
  * Once a key is revealed as belonging to a specific sidechain, then Unmasked Participants can vote on
@@ -240,6 +245,11 @@ interface SidechainAnonPinningInterface {
      */
     function getMaskedSidechainParticipant(uint256 _sidechainId, uint256 _index) external view returns(uint256);
 
+    /*
+     * Return the number of blocks between when a pin is posted and when it can be contested.
+     */
+    function getPinDisputePeriod() external view returns (uint32);
+
 
 
     event AddedSidechain(uint256 _sidechainId);
@@ -250,5 +260,6 @@ interface SidechainAnonPinningInterface {
     event VoteResult(uint256 _sidechainId, uint16 _action, uint256 _voteTarget, bool _result);
 
     event Dump1(uint256 a, uint256 b, address c);
+    event Dump2(uint256 a, uint256 b, uint256 c);
 
 }
